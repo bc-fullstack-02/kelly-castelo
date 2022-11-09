cc29e379d50040dfadc83c7f2d2e03ed3e9c113a
zam0k <kawokkun@gmail.com>
2022-11-09 11:30:12 -0300

    example of next function use

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,22 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.use("/static", express.static(path.join(__dirname, "static")));
-
-
 // app.get("/", (req, res) => {
 //     const content = fs.readFileSync(path.join(__dirname, 'static', 'index.html'), 'utf8');
 //     res.send(content);
 // })
+
+app.get("/", (req, res, next) => {
+    console.log("Primeiro get")
+    next()
+})
+
+app.get("/", (req, res, next) => {
+    console.log("Segundo get")
+    next()
+})
+
+app.use("/", express.static(path.join(__dirname, "static")));
 
 app.listen(3000, () => {
     console.log("Listening on http://localhost:3000")
