4573af936c98519102ee6b69230105ba9a08be79
zam0k <kawokkun@gmail.com>
2022-11-09 11:21:10 -0300

    displaying a static page with express

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
-})
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+
+// app.get("/", (req, res) => {
+//     const content = fs.readFileSync(path.join(__dirname, 'static', 'index.html'), 'utf8');
+//     res.send(content);
+// })
 
 app.listen(3000, () => {
     console.log("Listening on http://localhost:3000")
