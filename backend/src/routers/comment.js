16daaf9ae5ffd2af5eab974c9581c5768f4cd993
zam0k <kawokkun@gmail.com>
2022-11-19 16:34:10 -0300

    refactor comment routes

--- backend/src/routers/comment.js
+++ backend/src/routers/comment.js
@@ -12,7 +12,11 @@ commentRouter
   .route("/:postId/comments")
   .get((req, res) =>
     Promise.resolve()
-      .then(() => Comment.find({ post: req.params.postId }))
+      .then(() =>
+        Comment.find({ post: req.params.postId })
+          .select("-post")
+          .populate("user", ["-following", "-password"])
+      )
       .then((data) => res.status(200).json(data))
       .catch((err) => next(err))
   )
@@ -23,6 +27,7 @@ commentRouter
       )
       .then((comment) =>
         Post.findById(comment.post)
+          .populate("user", ["-following", "-password"])
           .then((post) =>
             Object.assign(post, { comments: [...post.comments, comment._id] })
           )
@@ -37,13 +42,16 @@ commentRouter
 
 // loading infinito se passar um id existente que não seja do post....
 commentRouter
-  .route("/:postId/comments/:id")
+  .route("/:postId/comments/:id") // TO-DO: is there a way to validate postId before all paths at once?
   .get((req, res, next) =>
     Promise.resolve()
-      .then(() => Comment.findById(req.params.id))
-      .then((data) => {
-        data ? res.status(200).json(data) : createError(404);
-      })
+      .then(() =>
+        Comment.findById(req.params.id)
+          .orFail(() => createError(404))
+          .select("-post")
+          .populate({ path: "user", select: ["-following", "-password"] })
+      )
+      .then((data) => res.status(200).json(data))
       .catch((err) => next(err))
   )
   .put((req, res, next) =>
@@ -51,16 +59,18 @@ commentRouter
       .then(() =>
         Comment.findByIdAndUpdate(req.params.id, req.body, {
           runValidators: true,
-          new: true
+          new: true,
         })
       )
-      .then((data) => res.status(203).json(data))
+      .then((data) => (data ? res.status(204).json() : next(createError(404))))
       .catch((err) => next(err))
   )
   .delete((req, res, next) =>
     Promise.resolve()
-      .then(() => Comment.deleteOne({ _id: req.params.id }))
-      .then((data) => res.status(203).json(data))
+      .then(() =>
+        Comment.deleteOne({ _id: req.params.id }).orFail(() => next(createError(404)))
+      )
+      .then(() => res.status(204).json())
       .catch((err) => next(err))
   );
 
