75c781e3500df3a872eb3403f7e5cb884a80fc53
zam0k <kawokkun@gmail.com>
2022-11-21 15:42:23 -0300

    add new routes to index

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,3 +1,6 @@
 exports.PostRouter = require("./post");
 exports.CommentRouter = require("./comment");
 exports.UserRouter = require("./user");
+exports.SecurityRouter = require("./security");
+exports.ProfileRouter = require("./profile");
+exports.FeedRouter = require("./feed");
