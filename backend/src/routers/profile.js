d2af3b3e6cef9ab9b9b18417ded9a5b00a4d4a32
zam0k <kawokkun@gmail.com>
2022-11-21 15:40:39 -0300

    refactor user and create new entity profile

---
+++ backend/src/routers/profile.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const profileRouter = express.Router();
+const createError = require("http-errors");
+
+const { Profile } = require("../models");
+
+profileRouter.route("/").get((req, res, next) =>
+  Promise.resolve()
+    .then(() => Profile.find({}))
+    .then((data) => res.status(200).json(data))
+    .catch((err) => next(err))
+);
+
+//to-do: review mongodb score parameters
+profileRouter.route("/search").get((req, res, next) =>
+  Promise.resolve()
+    .then(() =>
+      Profile.find(
+        { $text: { $search: `${req.query.q}` } },
+        { score: { $meta: "textScore" } }
+      ).sort({ score: { $meta: "textScore" } })
+    )
+    .then((data) =>
+      data ? res.status(200).json(data) : next(createError(404))
+    )
+    .catch((err) => next(err))
+);
+
+profileRouter.route("/:id").get((req, res, next) =>
+  Promise.resolve()
+    .then(() =>
+      Profile.findById(req.params.id).populate(["following", "followers"])
+    )
+    .then((data) =>
+      data ? res.status(200).json(data) : next(createError(404))
+    )
+    .catch((err) => next(err))
+);
+
+profileRouter.route("/:id/follow").post((req, res, next) =>
+  Promise.resolve()
+    .then(() => Profile.findById(req.params.id))
+    .then((data) =>
+      data.followers.find((user) => user._id.toString() === req.user.profile.id)
+    )
+    .then((user) =>
+      user === undefined
+        ? Profile.findOneAndUpdate(
+            { _id: req.params.id },
+            { $push: { followers: req.user.profile.id } }
+          )
+        : next(createError(400))
+    )
+    .then((data) =>
+      data
+        ? Profile.findOneAndUpdate(
+            { _id: req.user.profile.id },
+            { $push: { following: req.params.id } },
+            { new: true }
+          )
+        : next(createError(400))
+    )
+    .then((data) => (data ? res.status(200).json(data) : createError(400)))
+    .catch((err) => next(err))
+);
+
+profileRouter.route("/:id/unfollow").post((req, res, next) =>
+  Promise.resolve()
+    .then(() => Profile.findById(req.params.id))
+    .then((data) =>
+      data.followers.find((user) => user._id.toString() === req.user.profile.id)
+    )
+    .then((user) =>
+      user !== undefined
+        ? Profile.findOneAndUpdate(
+            { _id: req.params.id },
+            { $pull: { followers: req.user.profile.id } }
+          )
+        : next(createError(400))
+    )
+    .then((data) =>
+      data
+        ? Profile.findOneAndUpdate(
+            { _id: req.user.profile.id },
+            { $pull: { following: req.params.id } },
+            { new: true }
+          )
+        : next(createError(400))
+    )
+    .then((data) => (data ? res.status(200).json(data) : createError(400)))
+    .catch((err) => next(err))
+);
+
+module.exports = profileRouter;
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,51 +1,19 @@
 const express = require("express");
 const userRouter = express.Router();
-const createError = require("http-errors");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
 const { User } = require("../models");
-const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 userRouter
-  .route("/")
-  .get((req, res) =>
-    Promise.resolve()
-      .then(() => User.find({}, ["-password"]))
-      .then((data) => res.status(200).json(data))
-      .catch((err) => next(err))
-  )
-  .post((req, res, next) =>
-    Promise.resolve()
-      .then(() => bcrypt.hash(req.body.password, 8))
-      .then((password) => new User({ ...req.body, password }).save())
-      .then((data) => {
-        const user = { ...data };
-        delete user._doc.password;
-        return user._doc;
-      })
-      .then((user) => res.status(201).json(user))
-      .catch((err) => next(err))
-  );
-
-userRouter
-  .route("/:id")
+  .route("/me")
   .get((req, res, next) =>
     Promise.resolve()
-      .then(() =>
-        User.findById(req.params.id).select("-password").populate({
-          path: "following",
-        })
-      )
-      .then((data) =>
-        data ? res.status(200).json(data) : next(createError(404))
-      )
+      .then(() => User.findById(req.user.id, ["-password"]))
+      .then((data) => res.status(200).json(data))
       .catch((err) => next(err))
   )
   .put((req, res, next) =>
     Promise.resolve()
       .then(() =>
-        User.findByIdAndUpdate(req.params.id, req.body, {
+        User.findByIdAndUpdate(req.user.id, req.body, {
           runValidators: true,
           new: true,
           select: "-password",
@@ -56,26 +24,9 @@ userRouter
   )
   .delete((req, res, next) =>
     Promise.resolve()
-      .then(() => User.deleteOne({ _id: req.params.id }))
+      .then(() => User.deleteOne({ _id: req.user.id }))
       .then((data) => res.status(203).json(data))
       .catch((err) => next(err))
   );
-
-userRouter.route("/login").post((req, res, next) =>
-  Promise.resolve()
-    .then(() => User.findOne({ user: req.body.user }))
-    .then((user) =>
-      user
-        ? bcrypt.compare(req.body.password, user.password)
-        : next(createError(404))
-    )
-    .then((password) =>
-      password ? jwt.sign(req.body.user, TOKEN_SECRET) : next(createError(400))
-    )
-    .then((token) =>
-      token ? res.status(201).json({ token }) : next(createError(400))
-    )
-    .catch((err) => next(err))
-);
 
 module.exports = userRouter;
