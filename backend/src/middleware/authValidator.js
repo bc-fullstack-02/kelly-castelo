6f78310b4197e8ec447fc8755ad44cf8e0cc74ff
zam0k <kawokkun@gmail.com>
2022-11-21 15:39:31 -0300

    add forgotten next() to line 9

--- backend/src/middleware/authValidator.js
+++ backend/src/middleware/authValidator.js
@@ -6,10 +6,12 @@ const authValidator = (req, res, next) =>
   Promise.resolve()
     .then(() => req.headers.authorization)
     .then((authHeader) =>
-      authHeader ? authHeader.split(" ") : createError(401)
+      authHeader ? authHeader.split(" ") : next(createError(401))
     )
     .then(([_, token]) => jwt.verify(token, TOKEN_SECRET))
+    .then((token) => (req.user = token))
     .then(() => next())
     .catch((err) => next(err));
 
 module.exports = authValidator;
+
